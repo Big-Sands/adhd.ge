8d981c1259d5afd66918eca5e0a2a7b1a9b071de
Big-Sands <info@adhd.ge>
2023-02-27 22:35:15 +0400

    add multilingual support

--- render.py
+++ render.py
@@ -6,8 +6,30 @@ import time
 
 import pytz
 
-with open("contents.md", "r") as contents_file:
-    contents = contents_file.read()
+
+articles = [
+    {
+        "export_filename": "index.html",
+        "lang": "ka",
+        "title": "ADHD საქართველოში - ყურადღების დეფიციტის ჰიპერაქტიურობის აშლილობა (ადჰდ)",
+        "description": "ერთად გავზარდოთ ცნობიერება ADHD-ს მიმართ საქართველოს ჯანდაცვის სისტემაში!",
+        "last_update_str": "გვერდის ბოლო განახლების თარიღი:",
+        "file": "contents.md",
+        "canonical": "https://adhd.ge/",
+        "initial_publish_date": "2022-12-03T21:52:42.449005+04:00"
+    },
+    {
+        "export_filename": "index.en.html",
+        "lang": "en",
+        "title": "ADHD in Georgia - Attention deficit hyperactivity disorder",
+        "description": "Let's raise awareness of ADHD in the Georgian healthcare system together!",
+        "last_update_str": "Page last updated on:",
+        "file": "contents.en.md",
+        "canonical": "https://adhd.ge/index.en.html",
+        "initial_publish_date": "2022-12-03T21:52:42.449005+04:00"
+    }
+]
+
 
 with open("template.html", "r") as template_file:
     template = template_file.read()
@@ -20,19 +42,43 @@ extras = {
     "target-blank-links": None
 }
 
-rendered_contents = markdown2.markdown(contents, extras=extras)
+for article in articles:
+    with open(article["file"], "r") as contents_file:
+        contents = contents_file.read()
 
-timestamp = datetime.fromtimestamp(time.time(), pytz.timezone("Asia/Tbilisi"))
-expired_timestamp = timestamp + timedelta(hours=175320)
+    rendered_contents = markdown2.markdown(contents, extras=extras)
 
-initial_publish_date_str = "2022-12-03T21:52:42.449005+04:00"
+    timestamp = datetime.fromtimestamp(time.time(), pytz.timezone("Asia/Tbilisi"))
+    expired_timestamp = timestamp + timedelta(hours=175320)
 
-rendered_page = (template.replace("{% block content %}{% endblock %}", rendered_contents)
-                 .replace("{{ last_update_readable }}", timestamp.strftime("%Y-%m-%d %H:%M:%S (UTC%Z)"))
-                 .replace("{{ published_timestamp.isoformat() }}", initial_publish_date_str)
-                 .replace("{{ modified_timestamp.isoformat() }}", timestamp.isoformat())
-                 .replace("{{ expiration_timestamp.isoformat() }}", expired_timestamp.isoformat()))
+    rendered_page = (template.replace("{% block content %}{% endblock %}", rendered_contents))
 
+    metadata = {
+        "LAST_UPDATE_READABLE": timestamp.strftime("%Y-%m-%d %H:%M:%S (UTC%Z)"),
+        "METADATA_PUBLISHED_TIMESTAMP": article["initial_publish_date"],
+        "METADATA_MODIFIED_TIMESTAMP": timestamp.isoformat(),
+        "METADATA_EXPIRATION_TIMESTAMP": expired_timestamp.isoformat(),
+        "METADATA_LANG": article["lang"],
+        "METADATA_TITLE": article["title"],
+        "METADATA_CANONICAL_URL": article["canonical"],
+        "METADATA_DESCRIPTION": article["description"],
+        "LANG_LAST_UPDATE_STR": article["last_update_str"],
+        "HTML_EXTRA_NAV": ""
+    }
 
-with open("prod/index.html", "w") as rendered_file:
-    rendered_file.write(rendered_page)
+    extra_navs = {
+        "index.html": "<li class=\"nav-item\"><a href=\"/\" class=\"nav-link\">🇬🇪 ქართული</a></li>",
+        "index.en.html": "<li class=\"nav-item\"><a href=\"/index.en.html\" class=\"nav-link\">🇬🇧 English</a></li>"
+    }
+
+    for FILENAME, HTML in extra_navs.items():
+        if not FILENAME == article["export_filename"]:
+            metadata["HTML_EXTRA_NAV"] += HTML
+
+    for TAG, VALUE in metadata.items():
+        rendered_page = rendered_page.replace("{{ " + TAG + " }}", VALUE)
+
+    with open(f"prod/{article['export_filename']}", "w") as rendered_file:
+        rendered_file.write(rendered_page)
+
+print("articles generated!")
